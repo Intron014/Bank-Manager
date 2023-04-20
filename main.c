71c49edeb326bc43d01f2a5ba03fc6305dd5c905
Jorge Benjumea Burillo <me@intron014.com>
2023-04-20 18:01:36 +0200

    Starting structs

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
+typedef struct card{
+	int type; //1 -> Debit; 2 -> Credit; 0 -> Disabled;
+}CARD;
+
+typedef struct transactions{
+	int id;
+	char description[155];
+	float value;
+}TRANS;
+
+typedef struct account{
+	int accnumber; 
+	int accholder; 
+	int accid[9];
+	float accmoney;
+	CARD card;
+	TRANS transactions;
+}ACCOUNT;
+
+typedef struct bank{
+	ACCOUNT user;
+}BANK;
 
 void main(){
-
+	
 }
